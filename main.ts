2089273bd8cc683645f723ff5262eaef1b6643b2
kurtdevocht <kurt.de.vocht@telenet.be>
2026-03-09 09:36:17 +0100

    Added led bar + increased responsiveness

--- main.ts
+++ main.ts
@@ -3,11 +3,16 @@ let IndicatorLevelFrom0To100 = 0
 let ServoAngle = 0
 basic.forever(function () {
     NoiseLevelFrom0To100 = input.soundLevel() / 2.55
-    IndicatorLevelFrom0To100 = Math.max(NoiseLevelFrom0To100, IndicatorLevelFrom0To100 - 3)
+    IndicatorLevelFrom0To100 = Math.max(NoiseLevelFrom0To100, IndicatorLevelFrom0To100 - 5)
+    led.plotBarGraph(
+    IndicatorLevelFrom0To100,
+    100,
+    true
+    )
     if (IndicatorLevelFrom0To100 > 20) {
-        ServoAngle = 120 + 0.55 * IndicatorLevelFrom0To100
+        ServoAngle = 110 + 0.69 * IndicatorLevelFrom0To100
     } else {
-        ServoAngle = 120
+        ServoAngle = 110
     }
     servos.P0.setAngle(ServoAngle)
 })
